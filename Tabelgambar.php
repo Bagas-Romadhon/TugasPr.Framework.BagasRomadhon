8a85be5b98e763a79611ebb3ec8d41855688f5d0
Bagas-Romadhon <114625295+Bagas-Romadhon@users.noreply.github.com>
2022-09-29 21:34:06 +0700

    Add files via upload

---
+++ Tabelgambar.php
@@ -0,0 +1,43 @@
+ <!DOCTYPE html>
+<html>
+<head>
+	<title>Tugas Framework Bagas Romadhon</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<h1>Tugas Framework Bagas Romadhon</h1>
+	<table class="table1">
+       
+		<tr>
+			<th>Foto Barang</th>
+			<th>Nama Barang</th>
+            <th>Kode Barang</th>
+            <th>Detail Barang</th>
+			<th>Harga</th>
+			<th>Jasa pengiriman</th>
+            </tr>
+		<tr>
+			<td> <img src="laptop.jpg" alt="">< </td>
+			<td>Msi Modern 14 i3</td>
+            <td>MSI B11mou</td>
+			<td>14 inch ,256 SSD-NVME,Body Alloy,4000mah </td>
+			<td>Rp. 8.999.000,-</td>
+            <td> Jne ,J&T dan Take Away</td>
+		</tr>
+		<tr>
+        <td> <img src="acer.png" alt="">< </td>
+			<td>Acer aspire 4738</td>
+            <td>echo "Aspire 4738"</td>
+			<td>14 inch ,500 GB HDD,3000mah</td>
+			<td>Rp. 8.999.000,-</td>
+            <td> Jne ,J&T dan Take Away</td>
+		</tr>
+		<?php
+        echo  "Bagas Romadhon 182520061<br>";
+        echo  "Praktikum Pemrograman Frame work";
+
+        ?>
+	</table>	
+</body>
+</html>
+
